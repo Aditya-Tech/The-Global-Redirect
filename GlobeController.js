5fd1316c9d9ca5dbc99bc17cbb2cf26e7a79a3f1
Aditya Paruchuri <adityaparuchuri@Adityas-MacBook-Pro.local>
2017-06-03 11:27:04 -0400

    nevermind, that didn't work lol. Changing back

--- GlobeController.js
+++ GlobeController.js
@@ -1,10 +1,10 @@
-var worldFeed = "https://feeds.reuters.com/Reuters/worldNews?format=xml";
-var artsFeed = "https://feeds.reuters.com/news/artsculture";
-var techFeed = "https://feeds.reuters.com/reuters/technologyNews";
-var usFeed = "https://feeds.reuters.com/Reuters/domesticNews?format=xml";
-var popFeed = "https://feeds.reuters.com/reuters/oddlyEnoughNews";
-var envFeed = "https://feeds.reuters.com/reuters/environment";
-var cnnTest = "https://rss.cnn.com/rss/cnn_world.rss";
+var worldFeed = "http://feeds.reuters.com/Reuters/worldNews?format=xml";
+var artsFeed = "http://feeds.reuters.com/news/artsculture";
+var techFeed = "http://feeds.reuters.com/reuters/technologyNews";
+var usFeed = "http://feeds.reuters.com/Reuters/domesticNews?format=xml";
+var popFeed = "http://feeds.reuters.com/reuters/oddlyEnoughNews";
+var envFeed = "http://feeds.reuters.com/reuters/environment";
+var cnnTest = "http://rss.cnn.com/rss/cnn_world.rss";
 
 
 module.exports = {
